bebc56c526660829cb3a1eee1be85b72a1e8b349
alishaiaz <shaiazali@gmail.com>
2023-05-21 23:09:50 +0600

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+import Users from "./components/Users";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Users />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Users.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Users extends Component {
+  state = {
+    users: {},
+    x: "asdfas",
+  };
+
+  handleClick = async () => {
+    console.log(this.state.users);
+  };
+
+  async componentDidMount() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+
+    this.setState({ users });
+
+    console.log(users);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Users</h1>
+        <button onClick={this.handleClick}>Click</button>
+        <br />
+
+        {/* {this.state.users ? this.state.users : ""} */}
+        {this.state.x}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Users;
